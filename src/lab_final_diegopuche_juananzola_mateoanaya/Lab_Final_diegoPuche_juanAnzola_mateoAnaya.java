c44b148ff974a5fcbf54649c4e1c64ccbe907b90
DgxProjects29 <inevaup@gmail.com>
2020-05-27 10:24:40 -0500

    Base V 1.1

--- src/lab_final_diegopuche_juananzola_mateoanaya/Lab_Final_diegoPuche_juanAnzola_mateoAnaya.java
+++ src/lab_final_diegopuche_juananzola_mateoanaya/Lab_Final_diegoPuche_juanAnzola_mateoAnaya.java
@@ -1,96 +1,125 @@
 
 package lab_final_diegopuche_juananzola_mateoanaya;
 
-import java.util.Scanner;
 import javax.swing.JOptionPane;
-
 
 public class Lab_Final_diegoPuche_juanAnzola_mateoAnaya {
 
     
     public static void main(String[] args) {
        
-        Object op;
-        int finalop; 
-        Object selectedValue;
+        String option; 
   
         do {
             System.out.println("\n---------------------- Menú -------------------------");
             System.out.println("[1]. Triqui ");
-            System.out.println("[2]. Punto y fama: ");
+            System.out.println("[2]. Punto y fama ");
             System.out.println("[3]. Funciones trigonométricas con series de Taylor ");
             System.out.println("[4]. Clave de un número ");
             System.out.println("[5]. Multiplicación Rusa");
             System.out.println("[6]. Salir");
             System.out.println("-------------------------------------------------------");
             
-            Object[] possibleValues = {1, 2, 3, 4, 5, 6};
-            selectedValue = JOptionPane.showInputDialog(null,
-            "Escoge una opcion", "Ingrese:",
+            Object[] possibleValues = {"Triqui", "Punto y fama", 
+                "Funciones trigonométricas con series de Taylor", 
+                "Clave de un número", "Multiplicación Rusa", "Salir"};
+            Object optionFromPane = JOptionPane.showInputDialog(null,
+            "Escoge una opcion", "Menu:",
             JOptionPane.QUESTION_MESSAGE, null,
             possibleValues, possibleValues[0]);
-            op = selectedValue;
-           
-            while (op == null) {
-                
-                System.out.println("\n----------------------------------");
-                System.out.println("   No debes cancelar el dialogo  ");
-                System.out.println("------------------------------------");
-             
-                selectedValue = JOptionPane.showInputDialog(null,
-                "Escoge una opcion", "Menu:",
-                JOptionPane.QUESTION_MESSAGE, null,
-                possibleValues, possibleValues[0]);
-                op = selectedValue;
-
+            
+            //Si cacelas o cierras el JPaneOption, se acaba el programa
+            if(optionFromPane == null){
+                option = "";
+            }else{
+                option = (String)optionFromPane;
             }
             
-            finalop = (int)op;
-            
-            switch (finalop) {
+            switch (option) {
 
-                case 5:
+                case "Multiplicación Rusa":
                     
-                    //Depsues se agrega el joptionpane
-                    
+                    String inputValue;
+                    int multiplicador, multiplicando, result = 0;
+               
                     System.out.println("\n----------------------------");
                     System.out.println("    Multiplicacion Rusa     ");
                     System.out.println("-----------------------------");
                     
-                    Scanner l = new Scanner(System.in);
-                    int multiplicador, multiplicando, result = 0;
-
-                    System.out.print("Por favor digite el multiplicador:  ");
-                    multiplicador = l.nextInt();
+                    //Preguntar por el multiplicador
                     
-                    while (multiplicador <= 0) {
-                        System.out.println("[Error] Solo tabajamos con numeros naturales");
-                        System.out.print("Por favor digite el multiplicador:  ");
-                        multiplicador = l.nextInt();
+                    inputValue = JOptionPane.showInputDialog("Por favor, ingrese el multiplicador");
+
+                    while (inputValue == null || inputValue.equals("") || inputValue.equals("NoNatural")) {
+    
+                        inputValue = JOptionPane.showInputDialog("Por favor, ingrese el multiplicador");
+                        
+                        if(inputValue != null){
+                            
+                            if(!inputValue.equals("")){
+                                
+                                multiplicador = Integer.parseInt(inputValue);
+                                if(multiplicador <= 0){
+                                    System.out.println("[Error] Solo tabajamos con numeros naturales");
+                                    inputValue = "NoNatural";
+                                }
+                                
+                            }else{
+                                System.out.println("[Error] Debes ingresar algun valor");
+                            }
+ 
+                        }else{
+                            System.out.println("[Error] Debes ingresar algun valor");
+                        }
                     }
                     
-                    System.out.print("Por favor digite el multiplicando:  ");
-                    multiplicando = l.nextInt();
-                    
-                    while (multiplicando <= 0) {
-                        System.out.println("[Error] Solo tabajamos con numeros naturales");
-                        System.out.print("Por favor digite el multiplicando:  ");
-                        multiplicando = l.nextInt();
-                    }
+                    multiplicador = Integer.parseInt(inputValue);
 
-                    do {
-                       if (multiplicador % 2 != 0) {
+                    //Preguntar por el multiplicando
+                    
+                    inputValue = JOptionPane.showInputDialog("Por favor, ingrese el multiplicando");
+
+                    while (inputValue == null || inputValue.equals("") || inputValue.equals("NoNatural")) {
+    
+                        inputValue = JOptionPane.showInputDialog("Por favor, ingrese el multiplicando");
+                        
+                        if(inputValue != null){
+                            
+                            if(!inputValue.equals("")){
+                                
+                                multiplicando = Integer.parseInt(inputValue);
+                                if(multiplicador <= 0){
+                                    System.out.println("[Error] Solo tabajamos con numeros naturales");
+                                    inputValue = "NoNatural";
+                                }
+                                
+                            }else{
+                                System.out.println("[Error] Debes ingresar algun valor");
+                            }
+ 
+                        }else{
+                            System.out.println("[Error] Debes ingresar algun valor");
+                        }
+                    }
+                    
+                    multiplicando = Integer.parseInt(inputValue);
+                    
+                    while(multiplicador != 1){
+                        
+                        if (multiplicador % 2 != 0) {
                             result = result + multiplicando;
                         }
                         
                         System.out.println("Multiplicador: " + multiplicador + " "
                                 + "Multiplicando: "+multiplicando);
+                        
                         multiplicador = multiplicador / 2;
                         multiplicando = multiplicando * 2;
-                    } while (multiplicador != 1);
+                    }
                     
                     System.out.println("Multiplicador: " + multiplicador + " "
                                 + "Multiplicando: "+multiplicando);
+                    
                     //Uno siempre es impar
                     result += multiplicando;
                     
@@ -100,7 +129,7 @@ public class Lab_Final_diegoPuche_juanAnzola_mateoAnaya {
                     
             }
  
-        }while (finalop != 6);
+        }while (!option.equals(""));
     }
     
 }
